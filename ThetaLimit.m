da76bfdbc708f350558eed1e4a110ade31148bdd
lfrailev <lucasfraile@hotmail.com>
2015-12-08 22:45:13 -0500

    Update ThetaLimit.m

--- ThetaLimit.m
+++ ThetaLimit.m
@@ -1,7 +1,7 @@
 function [value,isterminal,direction] = ThetaLimit(t,y)
-% Locate the time when height passes through zero in a 
-% decreasing direction and stop integration.
-value = y(1)-pi()/3;     % Detect height = 0
+% Locate the time when theta passes through pi()/3  
+% and stop integration.
+value = y(1)-pi()/3;     % Detect theta=pi()/3
 isterminal = 1 ;   % Stop the integration
-direction = 1;   % Negative direction only
+direction = 1;   % Both Directions
 end
